946bdb02f4e07107b7fee681a3d8d24f9d418e34
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2016-09-19 16:17:05 -0300

    Added a directive for autogrowing an input field when it has the focus

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,18 +1,21 @@
 import {Component} from 'angular2/core'
 import {CoursesService} from './courses.service'
+import {AutoGrowDirective} from './auto-grow.directive'
 
 @Component({
   selector: 'courses',
   template: `
     <h2>Courses</h2>
     {{ title }}
+    <input type="text" autoGrow />
     <ul>
       <li *ngFor="#course of courses">
         {{ course }}
       </li>
     </ul>
     `,
-  providers: [CoursesService]
+  providers: [CoursesService],
+  directives: [AutoGrowDirective]
 })
 
 export class CoursesComponent {
